3261d196a221a8ef6098ab7e8e8b287921accad4
madizx <madis.sepp94@gmail.com>
2016-11-21 17:16:08 +0200

    update

--- src/class_01.java
+++ src/class_01.java
@@ -1,9 +1,18 @@
-
+import java.util.*;
 public class class_01 {
 	public static void main(String[] args) {
-	
+	//Toode_01 T1=new Toode_01(kogus, hind, kaivem);
+	Toode_01 T1=new Toode_01(5, 6, 1.2);
 
-		System.out.println("aega kulub ");
+	List<Ab_maks> tooted=new ArrayList<Ab_maks>();
+	tooted.add(T1);
+
+	for(Ab_maks l: tooted){
+	
+		System.out.println(l.myygiHind());
+}	
+
+
 		
 	}
 }
